ef58bc806f7f78f65bc7c8517fe1a7ed4e711b90
Erdem Tsynduev <erdemtsynduev@gmail.com>
2020-06-23 01:51:50 +0300

    feat: Add logic play all sound sample

--- buttons/src/main/java/com/erdemtsynduev/buttons/ButtonsViewModel.kt
+++ buttons/src/main/java/com/erdemtsynduev/buttons/ButtonsViewModel.kt
@@ -4,13 +4,40 @@ import android.content.Context
 import android.media.MediaPlayer
 import androidx.lifecycle.ViewModel
 
-class ButtonsViewModel(private val contextData: Context) : ViewModel() {
+class ButtonsViewModel(contextData: Context) : ViewModel() {
 
-    fun playSound() = playTuturu()
+    var mediaPlayerDejavu: MediaPlayer? = null
+    var mediaPlayerJojo: MediaPlayer? = null
+    var mediaPlayerMemeCredit: MediaPlayer? = null
+    var mediaPlayerMessageSteam: MediaPlayer? = null
+    var mediaPlayerMgsWarning: MediaPlayer? = null
+    var mediaPlayerNani: MediaPlayer? = null
+    var mediaPlayerNyaa: MediaPlayer? = null
+    var mediaPlayerRoblox: MediaPlayer? = null
+    var mediaPlayerTuturu: MediaPlayer? = null
+    var mediaPlayerYeah: MediaPlayer? = null
 
-    private fun playTuturu() {
-        val resId = contextData.resources.getIdentifier(R.raw.tuturu.toString(), "raw", contextData.packageName)
-        val mediaPlayer = MediaPlayer.create(contextData, resId)
-        mediaPlayer.start()
+    init {
+        mediaPlayerDejavu = MediaPlayer.create(contextData, R.raw.deja_vu)
+        mediaPlayerJojo = MediaPlayer.create(contextData, R.raw.jojo_reference)
+        mediaPlayerMemeCredit = MediaPlayer.create(contextData, R.raw.meme_de_creditos_finales)
+        mediaPlayerMessageSteam = MediaPlayer.create(contextData, R.raw.message_steam)
+        mediaPlayerMgsWarning = MediaPlayer.create(contextData, R.raw.mgs_warning)
+        mediaPlayerNani = MediaPlayer.create(contextData, R.raw.nani)
+        mediaPlayerNyaa = MediaPlayer.create(contextData, R.raw.nyaa)
+        mediaPlayerRoblox = MediaPlayer.create(contextData, R.raw.roblox_death_sound_effect)
+        mediaPlayerTuturu = MediaPlayer.create(contextData, R.raw.tuturu)
+        mediaPlayerYeah = MediaPlayer.create(contextData, R.raw.yeah)
     }
+
+    fun playSoundDejavu() = mediaPlayerDejavu?.start()
+    fun playSoundJojo() = mediaPlayerJojo?.start()
+    fun playSoundMemeCredit() = mediaPlayerMemeCredit?.start()
+    fun playSoundMessageSteam() = mediaPlayerMessageSteam?.start()
+    fun playSoundMgsWarning() = mediaPlayerMgsWarning?.start()
+    fun playSoundNani() = mediaPlayerNani?.start()
+    fun playSoundNyaa() = mediaPlayerNyaa?.start()
+    fun playSoundRoblox() = mediaPlayerRoblox?.start()
+    fun playSoundTuturu() = mediaPlayerTuturu?.start()
+    fun playSoundYeah() = mediaPlayerYeah?.start()
 }
